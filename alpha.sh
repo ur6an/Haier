63e9eb5b0a51d06b36235ad995f8069b751ff542
ur6an <ur6ant@gmail.com>
2026-01-08 22:35:46 +0100

    Create alpha.sh

---
+++ alpha.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# =============================================================
+#            GŁÓWNA CZĘŚĆ SKRYPTU
+# =============================================================
+
+echo "Wklejam pliki z paczki beta"
+
+systemctl stop haier
+
+rm -rf /opt/haier/static
+rm -rf /opt/haier/templates
+rm /opt/haier/main.py
+cd /opt/haier
+curl -sL https://github.com/ur6an/Haier/raw/refs/heads/main/fixV4.1.3.tar.gz |tar -xzv
+
+echo "Podmiana zakończona"
+echo "Startuje usługę Haier..."
+systemctl start haier && echo "✅ OK: USŁUGA WYSTARTOWAŁĄ" || echo "⚠️ UWAGA: Wystąpił błąd podczas startu usługi."
