444f7a206cd3f51ecd1799fa6ce37b0903874d73
ur6an <ur6ant@gmail.com>
2026-01-23 11:45:55 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
 #            GŁÓWNA CZĘŚĆ SKRYPTU
 # =============================================================
 
-echo "Wklejam pliki z paczki test 1.4.2a2"
+echo "Wklejam pliki z paczki test 1.4.2a3"
 
 systemctl stop haier
 
